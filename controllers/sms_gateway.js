03f6ad7796f63ef7b151299f3af40c72e0fb81ed
Dhamayanthi <dhamasr@gmail.com>
2016-03-18 10:05:29 +0530

    sms gateway balance request added

--- controllers/sms_gateway.js
+++ controllers/sms_gateway.js
@@ -1,5 +1,6 @@
 var Gateway = require('../models/gateway_request');
 var Customer = require('../models/customer');
+var Config = require('../configs/config');
 exports.processRequest = function (req, res) {
     console.log("Sms gateway request");
     new Gateway(req.body).save(function (err, doc) {
@@ -30,12 +31,28 @@ exports.processRequest = function (req, res) {
 
             //check if customer already exist in our system
             Customer.findOne({mobile_number: doc.request_mobile_number}, function (error, customerInfo) {
-                doc.request_info = req.body;
+                doc.request_info = req.body.request_info;
+
 
                 if (customerInfo) {
 
-                    result.customerInfo = customerInfo;
+
                     result.message = "proccessing your request";
+                    console.log(doc.request_type);
+                    switch (doc.request_type){
+                        case Config.SMS_ROUTE.GET_BALANCE:
+                            console.log("proccesing your balance");
+                            result.message = "balance"+Config.SPLIT_CHAR+customerInfo.wallet.balance+Config.SPLIT_CHAR+customerInfo.locker_amount;
+                            result.mobile_number = customerInfo.mobile_number;
+                            break;
+                        case Config.SMS_ROUTE.TRANSFER:
+                            console.log("Processing your transfer");
+                            break;
+                        default :
+                            console.log("NO Request type found");
+                            break;
+
+                    }
                     doc.response_info = result;
                     doc.result = true;
                     doc.save();
@@ -45,7 +62,7 @@ exports.processRequest = function (req, res) {
                 else {
                     var errorResponse = {
                         "success": false,
-                        "error": {"code": 102, "message": "Sorry,Tracing you... Run..."}
+                        "error": {"code": 102, "message": "Download Imoney, Get better Response"}
                     };
                     doc.response_info = errorResponse;
                     doc.result = false;
