f86a6947cfe66a0f51ccfbd1bae44e834f7c92d6
Dhamayanthi <dhamasr@gmail.com>
2016-03-24 18:09:50 +0530

    update locker amount response changed

--- controllers/customer.js
+++ controllers/customer.js
@@ -144,8 +144,9 @@ exports.lockAmount = function(req,res){
             customer.save()
             var result = {};
             result.message = "Wallet Balance";
-            result.amount = customer.wallet.virtual_balance ;
-            result.locker_amount = customer.locker_amount;
+            result.walletBalance =customer.wallet.balance;
+            result.liquidBalance = customer.wallet.virtual_balance ;
+            result.lockedBalance = customer.locker_amount;
             var response = {"success": true, "result": result};
             res.send(response);
         }
